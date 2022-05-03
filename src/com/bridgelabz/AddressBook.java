0f24de0a1a4792ebb8385d74c1195dd36d588067
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-03 17:49:48 +0530

    Added: Program to edit the person's information

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -9,27 +9,39 @@ import org.json.simple.parser.JSONParser;
 public class AddressBook {
 	Scanner scanner = new Scanner(System.in);
 
-	/** gets user's info **/
-	void addAPerson() {
-		System.out.print("Enter first and last name: ");
-		String firstName = scanner.next();
-		String lastName = scanner.next();
+	/** Edits a person's information **/
+	void edit(JSONObject jsonObject, JSONArray bookArray, int i) {
+		System.out.println("What do you want to edit?");
+		System.out.println("1. Address");
+		System.out.println("2. Phone Number");
 		
-		System.out.print("Enter complete address: ");
-		scanner.nextLine();
-		String address = scanner.nextLine();
+		int choice = scanner.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.print("Enter address: ");
+			scanner.nextLine();
+			
+			String address = scanner.nextLine();
+			System.out.print("Enter city, state and zip: ");
+			String city = scanner.next();
+			String state = scanner.next();
+			String zip = scanner.next();
+			
+			jsonObject.put("Address", address);
+			jsonObject.put("City", city);
+			jsonObject.put("State", state);
+			jsonObject.put("Zip", zip);
+			break;
+
+		case 2:
+			System.out.println("Enter phone number: ");
+			String phoneNumber = scanner.next();
+			jsonObject.put("Phone Number", phoneNumber);
+			break;
+		}
+		bookArray.remove(i);
+		bookArray.add(i, jsonObject);
 		
-		System.out.print("Enter city, state and zip: ");
-		String city = scanner.next();
-		String state = scanner.next();
-		String zip = scanner.next();
-		
-		System.out.println("Enter phone number: ");
-		String phoneNumber = scanner.next();
-		
-		Person person = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
-		JSONObject jsonObject = person.toJsonObject();
-		
-		writeBook(jsonObject);
+		printWriter(bookArray);
+		System.out.println("Contact updated successfully");
 	}
-}
