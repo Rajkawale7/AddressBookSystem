3f925395a8dae3d658cf91d5b2c363efaaef7a01
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-03 17:42:55 +0530

    Added: Program to get contact info as per user's input

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -2,61 +2,34 @@ package com.bridgelabz;
 
 import java.io.*;
 import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class AddressBook {
 	Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("***Welcome to the Address Book Program***");
-		AddressBook addressBook = new AddressBook();
-		addressBook.askUser();
-
-}
-	/** Menu **/
-	void askUser() {
-		System.out.println("Select an action..");
-		System.out.println("1. Add a person");
-		System.out.println("2. Edit");
-		System.out.println("3. Delete a person");
-		System.out.println("4. Search a person");
-		System.out.println("5. Quit");
+	/** gets user's info **/
+	void addAPerson() {
+		System.out.print("Enter first and last name: ");
+		String firstName = scanner.next();
+		String lastName = scanner.next();
 		
-		int choice = scanner.nextInt();
-		switch(choice) {
-		case 1:
-			addAPerson();
-			System.out.println("\n");
-			askUser();
-			break;
-		case 2:
-			System.out.print("Enter first and last name of the person to edit the contact: ");
-			commonPart(choice);
-			System.out.println("\n");
-			askUser();
-			break;
-		case 3:
-			System.out.print("Enter first and last name of the person to delete the contact: ");
-			commonPart(choice);
-			System.out.println("\n");
-			askUser();
-			break;
-		case 4:
-			System.out.print("Enter first and last name of the person to search: ");
-			commonPart(choice);
-			System.out.println("\n");
-			askUser();
-			break;
-		default:
-			break;
-		}
-	}
-	private void commonPart(int choice) {
-		// TODO Auto-generated method stub
+		System.out.print("Enter complete address: ");
+		scanner.nextLine();
+		String address = scanner.nextLine();
 		
-	}
-	private void addAPerson() {
-		// TODO Auto-generated method stub
+		System.out.print("Enter city, state and zip: ");
+		String city = scanner.next();
+		String state = scanner.next();
+		String zip = scanner.next();
 		
+		System.out.println("Enter phone number: ");
+		String phoneNumber = scanner.next();
+		
+		Person person = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+		JSONObject jsonObject = person.toJsonObject();
+		
+		writeBook(jsonObject);
 	}
 }
-	
