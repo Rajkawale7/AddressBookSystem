2c0ecc054ef77908e902839fad2a31ee0b7df877
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-03 18:00:52 +0530

    Added: Program to Display the contact info

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -7,41 +7,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class AddressBook {
-	Scanner scanner = new Scanner(System.in);
-
-	/** Edits a person's information **/
-	void edit(JSONObject jsonObject, JSONArray bookArray, int i) {
-		System.out.println("What do you want to edit?");
-		System.out.println("1. Address");
-		System.out.println("2. Phone Number");
-		
-		int choice = scanner.nextInt();
-		switch (choice) {
-		case 1:
-			System.out.print("Enter address: ");
-			scanner.nextLine();
-			
-			String address = scanner.nextLine();
-			System.out.print("Enter city, state and zip: ");
-			String city = scanner.next();
-			String state = scanner.next();
-			String zip = scanner.next();
-			
-			jsonObject.put("Address", address);
-			jsonObject.put("City", city);
-			jsonObject.put("State", state);
-			jsonObject.put("Zip", zip);
-			break;
-
-		case 2:
-			System.out.println("Enter phone number: ");
-			String phoneNumber = scanner.next();
-			jsonObject.put("Phone Number", phoneNumber);
-			break;
-		}
-		bookArray.remove(i);
-		bookArray.add(i, jsonObject);
-		
-		printWriter(bookArray);
-		System.out.println("Contact updated successfully");
+	/** Displays person's record **/
+	void search(JSONObject jsonObject) {
+		System.out.println("Name:\n" + jsonObject.get("First Name") + " " + jsonObject.get("Last Name"));
+		System.out.println("Address:\n" + jsonObject.get("Address"));
+		System.out.print(jsonObject.get("City") + ", ");
+		System.out.print(jsonObject.get("State") + " - ");
+		System.out.println(jsonObject.get("Zip"));
+		System.out.println("Phone Number:\n" + jsonObject.get("Phone Number"));
 	}
+}
